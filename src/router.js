3e2a8b50c476a1f8807e6680fd21769038c896bc
lrf402788946 <402788946@qq.com>
2019-01-23 20:02:59 +0800

    created

--- src/router.js
+++ src/router.js
@@ -1,10 +1,10 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Index from './views/Index.vue';
-import LoginPage from './views/LoginPage.vue';
+import LoginPage from './views/loginPage.vue';
 import DeptIndex from './views/dept/Index.vue';
 import UserIndex from './views/user/Index.vue';
-import Details from './views/Details.vue';
+import Details from './views/details.vue';
 import RoleIndex from './views/role/Index.vue';
 import UserRoleIndex from './views/userRole/Index.vue';
 
